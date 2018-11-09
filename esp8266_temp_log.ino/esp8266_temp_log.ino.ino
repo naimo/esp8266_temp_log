1b4b9b051f97e62c67a5ef29136f43ee347bbaae
naimo <naimo@users.noreply.github.com>
2018-11-09 12:38:43 +0100

    working low power temp and humidity sensor

--- esp8266_temp_log.ino/esp8266_temp_log.ino.ino
+++ esp8266_temp_log.ino/esp8266_temp_log.ino.ino
@@ -3,92 +3,160 @@
 #include "DHTesp.h"
 #include <PubSubClient.h>
 
+//#define DEBUG
+
 #include "config.h"
+IPAddress ip(192,168,1,151);
+IPAddress dns(192,168,1,1);
+IPAddress gateway(192,168,1,1);   
+IPAddress subnet(255,255,255,0);   
 
 WiFiClient wificlient;
 PubSubClient client(wificlient);
 
 DHTesp dht;
 
-void setup() {
-  
-  // Start Serial
-  Serial.begin(115200);
-  delay(10);
- 
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+#define WIFI_ENABLE_MAGIC 0xF0F0
+#define WIFI_ENABLE_OFFSET 0
+
+uint32_t wifi_enable;
+
+#define LAST_MEASUREMENT_OFFSET 1
+
+void setup() {  
+  ESP.rtcUserMemoryRead(WIFI_ENABLE_OFFSET, (uint32_t*) &wifi_enable, sizeof(wifi_enable));
+
+  switch(wifi_enable)
+  {
+    // wifi mode
+    case WIFI_ENABLE_MAGIC:
+    {  
+      #ifdef DEBUG
+        // Start Serial
+        Serial.begin(115200);
+        delay(10);
+        // We start by connecting to a WiFi network
+        Serial.println();
+        Serial.print("Connecting to ");
+        Serial.println(ssid); 
+      #endif
+
+      WiFi.config(ip, gateway, subnet, dns);    
+      WiFi.begin(ssid, password);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+      }
+    
+      #ifdef DEBUG
+        Serial.println();
+        Serial.print("Wifi connected: ");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+      #endif
+    
+      client.setServer(mqtthost, 1883);
+    
+      while (!client.connected()) {
+        if ( client.connect("esp8266", mqttuser, mqttpass) ) {
+          #ifdef DEBUG
+            Serial.println( "mqtt connected [DONE]" );
+          #endif
+        } else {
+          #ifdef DEBUG
+            Serial.print( "[FAILED] [ rc = " );
+            Serial.print( client.state() );
+            Serial.println( " : retrying in 5 seconds]" );
+          #endif
+          // Wait 5 seconds before retrying
+          delay( 5000 );
+        }
+      }
+      
+      TempAndHumidity values;
+      ESP.rtcUserMemoryRead(LAST_MEASUREMENT_OFFSET, (uint32_t*) &values, sizeof(values));
+   
+      #ifdef DEBUG     
+        // Just debug messages
+        Serial.print( "Sending temperature and humidity : [" );
+        Serial.print( values.temperature, DEC );
+        Serial.print( "," );
+        Serial.print( values.humidity, DEC );
+        Serial.print( "]   -> " );
+      #endif
+      
+      String temperature = String(values.temperature);
+      String humidity = String(values.humidity);
+      
+      // Prepare a JSON payload string
+      String payload = "{";
+      payload += "\"temperature\":"; payload += temperature; payload += ",";
+      payload += "\"humidity\":"; payload += humidity;
+      payload += "}";
+    
+      // Send payload
+      char attributes[100];
+      payload.toCharArray( attributes, 100 );
+      client.publish( "v1/devices/me/telemetry", attributes );
+      #ifdef DEBUG
+        Serial.println( attributes );
+      #endif
+        
+      // make sure message sent
+      wificlient.flush();
+
+      wifi_enable = 0;
+      ESP.rtcUserMemoryWrite(WIFI_ENABLE_OFFSET, (uint32_t*) &wifi_enable, sizeof(wifi_enable));
+      ESP.deepSleep(10 * 1000000, WAKE_RF_DISABLED);
+    }
+    // measurement mode
+    default:
+    {
+      pinMode(D5, OUTPUT);
+      digitalWrite(D5, HIGH);
+      dht.setup(D6, DHTesp::DHT11); // D6
+
+      #ifdef DEBUG
+        // Start Serial
+        Serial.begin(115200);
+        delay(1000);
+      #endif
+      
+      TempAndHumidity values;    
+      // throw away first values;
+      values = dht.getTempAndHumidity();
+    
+      #ifdef DEBUG
+        Serial.print( "Thrown temperature and humidity : [" );
+        Serial.print( values.temperature, DEC );
+        Serial.print( "," );
+        Serial.print( values.humidity, DEC );
+        Serial.println( "]" );
+      #endif
+      
+      delay(2000);
+         
+      // Reading temperature and humidity
+      values = dht.getTempAndHumidity();
+    
+      // shut dht immediately
+      pinMode(D5, INPUT);
+
+      #ifdef DEBUG
+        Serial.print( "Kept temperature and humidity : [" );
+        Serial.print( values.temperature, DEC );
+        Serial.print( "," );
+        Serial.print( values.humidity, DEC );
+        Serial.println( "]" );
+      #endif
+      
+      ESP.rtcUserMemoryWrite(LAST_MEASUREMENT_OFFSET, (uint32_t*) &values, sizeof(values));
+            
+      wifi_enable = WIFI_ENABLE_MAGIC;
+      ESP.rtcUserMemoryWrite(WIFI_ENABLE_OFFSET, (uint32_t*) &wifi_enable, sizeof(wifi_enable));     
+      ESP.deepSleep(10 * 1000000, WAKE_RF_DEFAULT);
+    }
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  client.setServer(mqtthost, 1883 );
-  
-  dht.setup(6); // data pin 2
 }
 
 void loop() {
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Attempting to reconnect");
-    WiFi.begin(ssid, password);
-    delay(500);
-  }
-  
-  if ( !client.connected() ) {
-    reconnect();
-  }
-   
-  // Reading temperature and humidity
-  float h = dht.getHumidity();
-  // Read temperature as Celsius
-  float t = dht.getTemperature();
-
-  String temperature = String(t);
-  String humidity = String(h);
-
-  // Just debug messages
-  Serial.print( "Sending temperature and humidity : [" );
-  Serial.print( temperature ); Serial.print( "," );
-  Serial.print( humidity );
-  Serial.print( "]   -> " );
-
-  // Prepare a JSON payload string
-  String payload = "{";
-  payload += "\"temperature\":"; payload += temperature; payload += ",";
-  payload += "\"humidity\":"; payload += humidity;
-  payload += "}";
-
-  // Send payload
-  char attributes[100];
-  payload.toCharArray( attributes, 100 );
-  client.publish( "v1/devices/me/telemetry", attributes );
-  Serial.println( attributes );
-  
-  delay(10000);
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Connecting ...");
-    if ( client.connect("esp8266",mqttuser,mqttpass) ) {
-      Serial.println( "[DONE]" );
-    } else {
-      Serial.print( "[FAILED] [ rc = " );
-      Serial.print( client.state() );
-      Serial.println( " : retrying in 5 seconds]" );
-      // Wait 5 seconds before retrying
-      delay( 5000 );
-    }
-  }
 }
